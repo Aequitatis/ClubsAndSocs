aa4cb6cdfa09d26fe356f93a6394d8ba6f601f5b
Aequitatis <tim.charters@avrion.net>
2016-10-25 12:47:53 +0100

    Added application variables and secrets infrastructure

---
+++ CAS_API/appsettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAS_API
+{
+    public class appsettings
+    {
+		public string testAppSetting { get; set; }
+
+		public string test { get; set; }
+	}
+}
